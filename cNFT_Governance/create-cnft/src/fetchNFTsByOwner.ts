4550a7d07a43e9a73fc44597b71ed80b599e6db7
Hau Chu <hchoward1998@gmail.com>
2023-06-20 19:06:05 -0500

    find cNFT by Owner

---
+++ cNFT_Governance/create-cnft/src/fetchNFTsByOwner.ts
@@ -0,0 +1,50 @@
+import { loadPublicKeysFromFile } from "@/utils/helper";
+import { ReadApiConnection } from "@metaplex-foundation/js";
+import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
+import { Connection, Keypair } from "@solana/web3.js";
+import { getAssetsByOwner } from "@/utils/read-api";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+(async () => {
+    const client = Keypair.fromSecretKey(
+        Uint8Array.from(JSON.parse(process.env.PRIVATE_KEY ?? "[]"))
+    );
+    console.log("client: ", client.publicKey.toBase58());
+
+    const RPC_URL = process.env.RPC_URL ?? "";
+    if (!RPC_URL) {
+        return console.warn("Please set RPC_URL from Helius");
+    }
+    const connection = new Connection(RPC_URL, "confirmed");
+    const balance = await connection.getBalance(client.publicKey);
+    console.log("balance: ", balance);
+
+    let keys = loadPublicKeysFromFile();
+    if (!keys?.collectionMint || !keys?.treeAddress) {
+        return console.warn("Please create collection and tree first");
+    }
+
+    const treeAddress = keys.treeAddress;
+    const collectionMint = keys.collectionMint;
+    console.log("==== Local PublicKeys loaded ====");
+    console.log("Tree address:", treeAddress.toBase58());
+    console.log("Collection mint:", collectionMint.toBase58());
+
+    await getAssetsByOwner(connection, {
+        ownerAddress: client.publicKey.toBase58(),
+    }).then((res) => {
+        console.log("Total assets returned: ", res.total);
+
+        res.items
+            ?.filter((asset) => asset.compression.tree === treeAddress.toBase58())
+            .map((asset) => {
+                // display some info about the asset
+                console.log("assetId:", asset.id);
+                console.log("ownership:", asset.ownership);
+                console.log("compression:", asset.compression);
+                console.log("collection:", asset.grouping[0].group_value);
+            });
+    });
+})();
---
+++ cNFT_Governance/create-cnft/utils/read-api.ts
@@ -0,0 +1,124 @@
+import {
+    GetAssetProofRpcInput,
+    GetAssetProofRpcResponse,
+    GetAssetsByGroupRpcInput,
+    GetAssetsByOwnerRpcInput,
+    MetaplexError,
+    ReadApiAssetList,
+} from "@metaplex-foundation/js";
+import { Connection, PublicKey } from "@solana/web3.js";
+
+type JsonRpcParams<ReadApiMethodParams> = {
+    method: string;
+    id?: string;
+    params: ReadApiMethodParams;
+};
+
+type JsonRpcOutput<ReadApiJsonOutput> = {
+    result: ReadApiJsonOutput;
+};
+
+/** @group Errors */
+export class ReadApiError extends MetaplexError {
+    readonly name: string = "ReadApiError";
+    constructor(message: string, cause?: Error) {
+        super(message, "rpc", undefined, cause);
+    }
+}
+
+const callReadApi = async <ReadApiMethodParams, ReadApiJsonOutput>(
+    connection: Connection,
+    jsonRpcParams: JsonRpcParams<ReadApiMethodParams>
+): Promise<JsonRpcOutput<ReadApiJsonOutput>> => {
+    const response = await fetch(connection.rpcEndpoint, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            jsonrpc: "2.0",
+            method: jsonRpcParams.method,
+            id: jsonRpcParams.id ?? "rpd-op-123",
+            params: jsonRpcParams.params,
+        }),
+    });
+
+    return await response.json();
+};
+
+export async function getAssetProof(
+    connection: Connection,
+    assetId: PublicKey
+): Promise<GetAssetProofRpcResponse> {
+    const { result: proof } = await callReadApi<GetAssetProofRpcInput, GetAssetProofRpcResponse>(
+        connection,
+        {
+            method: "getAssetProof",
+            params: {
+                id: assetId.toBase58(),
+            },
+        }
+    );
+    if (!proof) throw new ReadApiError("No asset proof returned");
+
+    return proof;
+}
+
+export async function getAssetsByGroup(
+    connection: Connection,
+    { groupKey, groupValue, page, limit, sortBy, before, after }: GetAssetsByGroupRpcInput
+): Promise<ReadApiAssetList> {
+    // `page` cannot be supplied with `before` or `after`
+    if (typeof page == "number" && (before || after))
+        throw new ReadApiError(
+            "Pagination Error. Only one pagination parameter supported per query."
+        );
+
+    // a pagination method MUST be selected, but we are defaulting to using `page=0`
+
+    const { result } = await callReadApi<GetAssetsByGroupRpcInput, ReadApiAssetList>(connection, {
+        method: "getAssetsByGroup",
+        params: {
+            groupKey,
+            groupValue,
+            after: after ?? null,
+            before: before ?? null,
+            limit: limit ?? null,
+            page: page ?? 1,
+            sortBy: sortBy ?? null,
+        },
+    });
+
+    if (!result) throw new ReadApiError("No results returned");
+
+    return result;
+}
+
+export async function getAssetsByOwner(
+    connection: Connection,
+    { ownerAddress, page, limit, sortBy, before, after }: GetAssetsByOwnerRpcInput
+): Promise<ReadApiAssetList> {
+    // `page` cannot be supplied with `before` or `after`
+    if (typeof page == "number" && (before || after))
+        throw new ReadApiError(
+            "Pagination Error. Only one pagination parameter supported per query."
+        );
+
+    // a pagination method MUST be selected, but we are defaulting to using `page=0`
+
+    const { result } = await callReadApi<GetAssetsByOwnerRpcInput, ReadApiAssetList>(connection, {
+        method: "getAssetsByOwner",
+        params: {
+            ownerAddress,
+            after: after ?? null,
+            before: before ?? null,
+            limit: limit ?? null,
+            page: page ?? 1,
+            sortBy: sortBy ?? null,
+        },
+    });
+
+    if (!result) throw new ReadApiError("No results returned");
+
+    return result;
+}
