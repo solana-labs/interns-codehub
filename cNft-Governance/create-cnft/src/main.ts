f1cfe34cbebf5a5948f1b2580914951ddee06b2c
Hau Chu <hchoward1998@gmail.com>
2023-07-31 16:40:57 -0500

    fix read private key error

--- cNft-Governance/create-cnft/src/main.ts
+++ cNft-Governance/create-cnft/src/main.ts
@@ -1,66 +1,100 @@
 import { Keypair, Connection, PublicKey } from "@solana/web3.js";
+import { loadNonceFromFile, loadPublicKeysFromFile } from "@/utils/helper";
 import {
-    loadNonceFromFile,
-    loadPublicKeysFromFile,
-} from "@/utils/helper";
-import {
-    Metaplex,
-    bundlrStorage,
-    keypairIdentity,
+  Metaplex,
+  bundlrStorage,
+  keypairIdentity,
 } from "@metaplex-foundation/js";
 import { createNftCollection } from "./createCollection";
 import { createTree } from "./createTree";
 import { mintNft } from "./mintNft";
 import { mintCompressedNft } from "./mintCompressedNft";
+import dotenv from "dotenv";
+dotenv.config();
 
 (async () => {
-    const payer = Keypair.fromSecretKey(
-        Uint8Array.from(JSON.parse(process.env.PRIVATE_KEY ?? "[]"))
+  const payer = Keypair.fromSecretKey(
+    Uint8Array.from(JSON.parse(process.env.PRIVATE_KEY ?? "[]"))
+  );
+  console.log("payer: ", payer.publicKey.toBase58());
+
+  const connection = new Connection(process.env.RPC_URL ?? "", "confirmed");
+  const balance = await connection.getBalance(payer.publicKey);
+  console.log("balance: ", balance);
+
+  const metaplex = Metaplex.make(connection)
+    .use(keypairIdentity(payer))
+    .use(
+      bundlrStorage({
+        address: "https://devnet.bundlr.network",
+        providerUrl: "https://api.devnet.solana.com",
+        timeout: 60000,
+      })
     );
-    console.log("payer: ", payer.publicKey.toBase58());
 
-    const connection = new Connection(process.env.RPC_URL ?? "", "confirmed");
-    const balance = await connection.getBalance(payer.publicKey);
-    console.log("balance: ", balance);
+  let keys = loadPublicKeysFromFile();
+  if (
+    !keys?.collectionMint ||
+    !keys?.collectionMetadataAccount ||
+    !keys?.collectionMasterEditionAccount
+  ) {
+    await createNftCollection(payer, connection, metaplex);
+  }
 
-    const metaplex = Metaplex.make(connection)
-        .use(keypairIdentity(payer))
-        .use(
-            bundlrStorage({
-                address: "https://devnet.bundlr.network",
-                providerUrl: "https://api.devnet.solana.com",
-                timeout: 60000,
-            })
-        );
+  if (!keys?.treeAddress1 || !keys?.treeAuthority1) {
+    await createTree(payer, connection, 1, 2, {
+      maxDepth: 5,
+      maxBufferSize: 8,
+    });
+  }
 
-    let keys = loadPublicKeysFromFile();
-    if (!keys?.collectionMint || !keys?.collectionMetadataAccount || !keys?.collectionMasterEditionAccount) {
-        await createNftCollection(payer, connection, metaplex);
-    }
+  if (!keys?.treeAddress2 || !keys?.treeAuthority2) {
+    await createTree(payer, connection, 2, 8, {
+      maxDepth: 14,
+      maxBufferSize: 64,
+    });
+  }
 
-    if (!keys?.treeAddress1 || !keys?.treeAuthority1) {
-        await createTree(payer, connection, 1, 2, { maxDepth: 5, maxBufferSize: 8 });
-    }
+  keys = loadPublicKeysFromFile();
+  const treeAddress1: PublicKey = keys.treeAddress1;
+  const treeAuthority1: PublicKey = keys.treeAuthority1;
+  const treeAddress2: PublicKey = keys.treeAddress2;
+  const treeAuthority2: PublicKey = keys.treeAuthority2;
+  const collectionMint: PublicKey = keys.collectionMint;
+  const collectionMetadataAccount: PublicKey = keys.collectionMetadataAccount;
+  const collectionMasterEditionAccount: PublicKey =
+    keys.collectionMasterEditionAccount;
 
-    if (!keys?.treeAddress2 || !keys?.treeAuthority2) {
-        await createTree(payer, connection, 2, 4, { maxDepth: 14, maxBufferSize: 64 });
-    }
-    keys = loadPublicKeysFromFile();
-    const treeAddress1: PublicKey = keys.treeAddress1;
-    const treeAuthority1: PublicKey = keys.treeAuthority1;
-    const treeAddress2: PublicKey = keys.treeAddress2;
-    const treeAuthority2: PublicKey = keys.treeAuthority2;
-    const collectionMint: PublicKey = keys.collectionMint;
-    const collectionMetadataAccount: PublicKey = keys.collectionMetadataAccount;
-    const collectionMasterEditionAccount: PublicKey = keys.collectionMasterEditionAccount;
-
-    await mintNft(payer, metaplex, collectionMint, 6)
-    await mintNft(payer, metaplex, collectionMint, 7)
-    await mintCompressedNft(payer, connection, treeAddress1,
-        treeAuthority1, collectionMint, collectionMetadataAccount, collectionMasterEditionAccount, 8)
-    await mintCompressedNft(payer, connection, treeAddress1,
-        treeAuthority1, collectionMint, collectionMetadataAccount, collectionMasterEditionAccount, 9)
-    await mintCompressedNft(payer, connection, treeAddress2,
-        treeAuthority2, collectionMint, collectionMetadataAccount, collectionMasterEditionAccount, 10)
-
+  await mintNft(payer, metaplex, collectionMint, 6);
+  await mintNft(payer, metaplex, collectionMint, 7);
+  await mintCompressedNft(
+    payer,
+    connection,
+    treeAddress1,
+    treeAuthority1,
+    collectionMint,
+    collectionMetadataAccount,
+    collectionMasterEditionAccount,
+    11
+  );
+  await mintCompressedNft(
+    payer,
+    connection,
+    treeAddress1,
+    treeAuthority1,
+    collectionMint,
+    collectionMetadataAccount,
+    collectionMasterEditionAccount,
+    9
+  );
+  await mintCompressedNft(
+    payer,
+    connection,
+    treeAddress2,
+    treeAuthority2,
+    collectionMint,
+    collectionMetadataAccount,
+    collectionMasterEditionAccount,
+    10
+  );
 })();
